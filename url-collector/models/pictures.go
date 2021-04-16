6dc8f9531956563bd46f6c979d5e8c1617dce7ab
greg9702 <grzegorzb9702@gmail.com>
2021-04-16 17:09:30 +0200

    Added model for binding incoming GetImages request, added validation of dates and its order

---
+++ url-collector/models/pictures.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+	"errors"
+	"time"
+)
+
+type PicturesToBeFetched struct {
+	StartDate time.Time `form:"start_date" binding:"required" time_format:"2006-01-02"`
+	EndDate   time.Time `form:"end_date" binding:"required" time_format:"2006-01-02"`
+}
+
+func (p *PicturesToBeFetched) Validate() error {
+	if p.EndDate.Before(p.StartDate) {
+		return errors.New("End date is before start date")
+	}
+	return nil
+}
